aa2cff804f37c3acfe2d1a7b809b2f37efe9597b
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-10-12 22:27:18 -0300

    Add events to Add and Refres --> index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
-import _ from 'lodash';
+import { leaderboard, UpdateLocalStorage } from './modules/data.js';
+import {
+  addBtn, nameIn, refreshBtn, scoreIn,
+} from './modules/domlist.js';
+import writeList from './modules/innerhtml.js';
+import Leader from './modules/leaders.js';
 import './style.css';
+
+writeList();
+
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const leader = new Leader(nameIn.value, scoreIn.value);
+  leader.add();
+  writeList();
+  // nameIn.value = '';
+  // scoreIn.value = '';
+});
+
+refreshBtn.addEventListener('click', () => {
+  leaderboard.splice(0, leaderboard.length);
+  UpdateLocalStorage(leaderboard);
+  writeList();
+});
