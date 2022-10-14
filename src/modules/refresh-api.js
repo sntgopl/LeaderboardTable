7fb2e3545feccdfb59004d9608c58da032c6c69c
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-10-14 18:00:11 -0300

    Fecth list from API server function

---
+++ src/modules/refresh-api.js
@@ -0,0 +1,15 @@
+import createGame from './create-api.js';
+import { leaderboard } from './data.js';
+import writeList from './innerhtml.js';
+
+const keyGame = createGame();
+
+const refreshList = async () => {
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${keyGame}/scores`);
+  const data = await response.json();
+  leaderboard.push(data.result);
+  await writeList();
+  return data;
+};
+
+export default refreshList;
