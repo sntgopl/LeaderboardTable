3f2af5decdc92bdcbedfb05c7de9691adae8d256
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-10-14 17:59:29 -0300

    Create API game POST function

---
+++ src/modules/create-api.js
@@ -0,0 +1,16 @@
+const gameName = { name: 'Fidoxidido' };
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+
+const createGame = async () => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(gameName),
+  });
+  const data = await response.json();
+  return data.result.slice(14, 34);
+};
+
+export default createGame;
