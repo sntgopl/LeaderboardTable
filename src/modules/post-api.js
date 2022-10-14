5835214d25b5b7ba7d98bff5465ebc792117e485
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-10-14 18:01:15 -0300

    Create POST function to add new leaders

---
+++ src/modules/post-api.js
@@ -0,0 +1,34 @@
+import createGame from './create-api.js';
+
+const keyGame = createGame();
+
+const addLeader = async (newLeader) => {
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${keyGame}/scores/`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(newLeader),
+  });
+  const data = await response.json();
+  return data;
+};
+
+export default addLeader;
+// async function sendData(name, score) {
+//   const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/';
+//   const response = await fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-type': 'application/json; charset=UTF-8',
+//     },
+//     body: JSON.stringify({
+//       user: name,
+//       score,
+//     }),
+//   });
+//   const data = await response.json();
+//   return data;
+// }
+
+// export default sendData;
